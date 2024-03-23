2b17f226ce0330ff8c2ba66ea65c29e219d8f5df
Karan96Kaushik <karankshk96@gmail.com>
2024-03-23 21:41:37 +0000

    save password workaround

--- chrome/background.js
+++ chrome/background.js
@@ -1,8 +1,36 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    chrome.tabs.create({url: 'https://maharerait.mahaonline.gov.in/Login/LogOff'}, function(tab) {
+        chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo, updatedTab) {
+            if (tabId === tab.id && changeInfo.status === 'complete') {
+                chrome.tabs.onUpdated.removeListener(listener);
+                chrome.tabs.sendMessage(tab.id, request, function(response) {
+                    console.log(response);
+                })
+            }
+        });
+    })
+
+    if (false)
     chrome.tabs.query({currentWindow: true}, function(tabs) {
         let tab = tabs.find(t => t.title.includes('Login Page : Maharashtra'))
-        chrome.tabs.sendMessage(tab.id, request, function(response) {
-            console.log(response);
-        });
+        if (tab?.id)
+            chrome.tabs.sendMessage(tab.id, request, function(response) {
+                console.log(response);
+            })
+        else 
+            chrome.tabs.create({url: 'https://maharerait.mahaonline.gov.in'}, function(tab) {
+
+                chrome.tabs.onUpdated.addListener(function listener(tabId, changeInfo, updatedTab) {
+                    if (tabId === tab.id && changeInfo.status === 'complete') {
+                        chrome.tabs.onUpdated.removeListener(listener);
+                        chrome.tabs.sendMessage(tab.id, request, function(response) {
+                            console.log(response);
+                        })
+                    }
+                });
+
+
+
+            })
     });
 });
--- chrome/rera-content.js
+++ chrome/rera-content.js
@@ -5,10 +5,31 @@ chrome.runtime.onMessage.addListener(
 
 		if (userNameInput) {
 			userNameInput.value = request.userID;
+			userNameInput.type = 'password';
 		}
 		if (passwordInput) {
 			passwordInput.value = request.password;
 		}
 		sendResponse({message: "received"});
+
+		var form = document.querySelector('form'); // Adjust the selector to target your specific form
+		if (form) {
+	
+			form.addEventListener('submit', function(event) {
+				setTimeout(() => {
+					const userNameInput = document.querySelector('input[name="UserName"]');
+					if (userNameInput) {
+						userNameInput.value = "***HIDDEN***";
+					}
+					const passwordInput = document.querySelector('input[name="Password"]');
+					if (passwordInput) {
+						passwordInput.value = "***HIDDEN***";
+					}
+					
+				}, 0);
+	
+			});
+		}
+
 	}
 );
